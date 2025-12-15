c9c4dec4ec96d2666c0ccac0e93c7fcd83284405
aidenjay196 <aidenpace1496@gmail.com~>
2025-12-15 10:22:38 -0500

    finished javascript clock with customizable styles

---
+++ script.js
@@ -0,0 +1,33 @@
+function showTime(){
+    var date = new Date();
+    var h = date.getHours(); // 0 - 23
+    var m = date.getMinutes(); // 0 - 59
+    var s = date.getSeconds(); // 0 - 59
+    var session = "AM";
+
+    if(h == 0){
+        h = 12;
+    }
+
+    if(h > 12){
+        h = h - 12;
+        session = "PM";
+    }
+
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+
+    var time = h + ":" + m + ":" + s + " " + session;
+    document.getElementById("MyClockDisplay").innerText = time;
+
+    setTimeout(showTime, 1000);
+}
+
+// Function to change the clock's style by updating its class name
+function changeStyle(styleName) {
+    const clockElement = document.getElementById("MyClockDisplay");
+    clockElement.className = 'clock ' + styleName;
+}
+
+showTime();
